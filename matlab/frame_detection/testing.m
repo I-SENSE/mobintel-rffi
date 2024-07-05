4e5e7f0d201f10c24c8bda56a8c99e9030c1f73b
Stephan Mazokha <stepan.mazokha@gmail.com>
2024-07-05 11:17:06 -0700

    Fixed bug in multi-day node filtering

--- matlab/frame_detection/testing.m
+++ matlab/frame_detection/testing.m
@@ -1,4 +1,4 @@
-input_dir = '/Users/stepanmazokha/Desktop/wisig_raw_rffi_dataset/node1-1/';
-output_dir = '/Users/stepanmazokha/Desktop/wisig_frames_rffi_dataset/node1-1/';
+input_dir = '/Users/stepanmazokha/Desktop/wisig_raw_rffi_dataset/node1-1_wifi_2021_03_08/';
+output_dir = '/Users/stepanmazokha/Desktop/wisig_frames_rffi_dataset/node1-1_wifi_2021_03_08/';
 
 main_detect_frames('node1-1', input_dir, output_dir);
--- matlab/frame_equalization/testing.m
+++ matlab/frame_equalization/testing.m
@@ -1,4 +1,4 @@
-input_dir = '/Users/stepanmazokha/Desktop/wisig_frames_rffi_dataset/node1-1/';
-output_dir = '/Users/stepanmazokha/Desktop/wisig_frames_rffi_dataset/node1-1/';
+input_dir = '/Users/stepanmazokha/Desktop/wisig_frames_rffi_dataset/node1-1_wifi_2021_03_08/';
+output_dir = '/Users/stepanmazokha/Desktop/wisig_frames_rffi_dataset/node1-1_wifi_2021_03_08/';
 
-main_equalize_frames(input_dir, output_dir, 1000);
+main_equalize_frames(input_dir, output_dir, 500);
